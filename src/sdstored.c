aa6ec833b4f901c9a04067462a43a60fa797c3e1
RicAlvesO <ricaoimp@gmail.com>
2022-04-12 16:24:25 +0100

    Added file verification to server

--- src/sdstored.c
+++ src/sdstored.c
@@ -1,18 +1,50 @@
 // Programa servidor
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
 
+// Verify If Bin Files Exists
+int verifyBinFiles(char* path){
+    int flag = 1;
+    char *bins[]={"bcompress","bdecompress","decrypt","encrypt","gcompress","gdecompress","nop"};
+    for(int i=0; i<7; i++){
+    
+        // Create file destinations
+        char str[(sizeof(bins[i]) + sizeof(path)) / sizeof(char)] = "";
+        strcat(str, path);
+        strcat(str, bins[i]);
+        
+        // Verify file existance
+        if (access(str, F_OK) == -1){
+            
+            // Make ERROR message
+            char error[(sizeof(bins[i])/8) + 12] = "";
+            strcat(error, bins[i]);
+            strcat(error, "NOT FOUND!\n");
+            
+            // Print ERROR message
+            write(2, error, strlen(error));
+            flag=0;
+        }
+    }
+    return flag;
+}
+
+// Main Function
 int main(int argc, char* argv[]) {
+    
+    // Argument Verifications
     if (argc != 3){
         write(2, "NOT ENOUGH ARGUMENTS PROVIDED!\n", 32);
         return -1;
     }
-
+    // Config File Verification
     if (access(argv[1], F_OK) == -1){
         write(2, "CONFIG FILE NOT FOUND!\n", 24);
+        return -1;
     }
+    //Bin Files Verification
+    if(!verifyBinFiles(argv[2]))return -1;
 
-    //argv[1] -> caminho para ficheiro de configuração
-    //argv[2] -> caminho para a pasta onde os executáveis das transformações estão guardados
     return 1;
 }
